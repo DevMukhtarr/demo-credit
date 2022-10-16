77fbedaeeb718a3b68a4f91341d220b3a4f8ee1e
Your Name <you@example.com>
2022-10-17 00:41:34 +0100

    added latest compiled folder

--- dist/src/app.js
+++ dist/src/app.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const appConnect_1 = __importDefault(require("./appConnect"));
-const port = 3000;
+const APP_PORT = process.env.APP_PORT;
 appConnect_1.default.get('/', (req, res) => {
     res.send('Hello World!');
 });
-appConnect_1.default.listen(port, () => {
-    return console.log(`App is listening at Port ${port}`);
+appConnect_1.default.listen(APP_PORT, () => {
+    return console.log(`App is listening at Port ${APP_PORT}`);
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import app from './appConnect';
-const port = 3000;
+const APP_PORT = process.env.APP_PORT;
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  return console.log(`App is listening at Port ${port}`);
+app.listen(APP_PORT, () => {
+  return console.log(`App is listening at Port ${APP_PORT}`);
 });
